d269169407c85b27cf315e74cafbb75d706a8348
selen_erkan <erkan@agrilution.com>
2022-06-20 09:38:59 +0200

    add train loop

--- src/main.py
+++ src/main.py
@@ -1,13 +1,104 @@
-
 from src.model import *
 from src.dataset import *
 from torchsummary import summary
+from torch import optim, utils
+from smart_dataset import get_dataset
+from torch.utils.data import DataLoader
+from torch import permute
 
-root_dir = "/Users/goktug/PycharmProjects/ML4RG Project/data"
-filename = "train_sequences.txt"
-train_dataset = PromoterSeqDataset(root_dir, filename, transforms)
-loader = DataLoader(train_dataset, batch_size=64, collate_fn=collate_batch)
+# specify batch size, number of epochs, and learning rate
+BATCH_SIZE = 1
+EPOCHS = 10
+LR = 1e-2
+DATA_DIR = r'../data/train_sequences.txt'
+
+# determine the device being used for training
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print("[INFO] training using {}...".format(DEVICE))
 
 net = PromoterNet()
-summary(net, input_size=(5, 112))
-# for samples, labels in loader:
+# summary(net, input_size=(5, 112))
+loss_func = nn.MSELoss()
+optimizer = optim.Adam(net.parameters(),
+                       lr=LR)
+
+# get dataloaders
+tr_dataset = get_dataset(filename=DATA_DIR, max_sample_bytes=30000)
+train_loader = DataLoader(tr_dataset, batch_size=BATCH_SIZE)
+
+
+# root_dir = r"../data"
+# filename = "train_sequences.txt"
+# train_dataset = PromoterSeqDataset(root_dir, filename, transforms)
+# loader = DataLoader(train_dataset, batch_size=64, collate_fn=collate_batch)
+# for each in loader:
+#     print(each[0].size())
+
+
+def train_one_epoch(epoch_index):
+    running_loss = 0.
+    last_loss = 0.
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(train_loader):
+        print(i)
+        print(data)
+        # Every data instance is an sequences + label pair
+        sequences, labels = data["sequence"], data['expression']
+
+        # change the order of the input shapes, final: [batch_size, channels, depth, height, width]
+        sequences = permute(sequences, (0, 2, 1))
+        sequences = sequences.type(torch.float)
+
+        print(sequences.size())
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = net(sequences)
+
+        # Compute the loss and its gradients
+        loss = loss_func(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000  # loss per batch
+            print(' epoch {} batch {} loss: {}'.format(epoch_index, i + 1, last_loss))
+            # tb_x = epoch_index * len(train_loader) + i + 1
+            # tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+
+    return last_loss
+
+
+best_vloss = 1_000_000.
+for epoch in range(EPOCHS):
+    # get the loss
+    avg_loss = train_one_epoch(epoch)
+
+    # # get a validation loader and run this part later
+    # running_vloss = 0.0
+    # for i, vdata in enumerate(validation_loader):
+    #     vinputs, vlabels = vdata
+    #     voutputs = net(vinputs)
+    #     vloss = loss_func(voutputs, vlabels)
+    #     running_vloss += vloss
+    #
+    # avg_vloss = running_vloss / (i + 1)
+    #
+    # print('EPOCH {} LOSS train {} valid {}'.format(epoch, avg_loss, avg_vloss))
+    #
+    # # Track best performance, and save the model's state
+    # if avg_vloss < best_vloss:
+    #     best_vloss = avg_vloss
+    #     model_path = 'model_cnn_epoch{}'.format(epoch)
+    #     torch.save(net.state_dict(), model_path)
--- src/smart_dataset.py
+++ src/smart_dataset.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import monai
 import torch
+from kipoiseq.transforms.functional import one_hot, fixed_len
+
 
 class OneHotEncoding(monai.transforms.MapTransform):
     def __init__(self, keys):
@@ -10,35 +12,45 @@ class OneHotEncoding(monai.transforms.MapTransform):
         print(f"keys to square it: {self.keys}")
 
     def __call__(self, x):
-        output = {key: self.one_hot(x[key]) for key in self.keys}
+        # output = {key: self.one_hot(x[key]) for key in self.keys}
+        output = {}
+        for key in self.keys:
+            output[key] = self.one_hot(x[key])
+            output['expression'] = x['expression']
+        # print('output', output)
         return output
 
-    def one_hot(self,seq):
+    def one_hot(self, seq):
         seq = np.array(list(seq)).astype(np.int64)
         seq = torch.LongTensor(seq)
-        seq = torch.nn.functional.one_hot(seq,num_classes=5)
+        seq = torch.nn.functional.one_hot(seq, num_classes=5)
         return seq
 
-def get_dataset(filename="train_sequences.txt", max_sample_bytes=-1, replace_rate=0.2, cache_rate=0.02):
+
+def get_dataset(filename=r"../data/train_sequences.txt", max_sample_bytes=-1, replace_rate=0.2, cache_rate=0.02):
     f = open(filename, "r")
     lines = f.readlines(max_sample_bytes)
     f.close()
     tr_data = pd.DataFrame(lines)[0].str.split('\t', 1, expand=True)
-    tr_data[1] = tr_data[1].str.replace('\n',"").astype(np.float32)
+    tr_data[1] = tr_data[1].str.replace('\n', "").astype(np.float32)
     tr_data[0] = tr_data[0].str[17:-13]
-    tr_data[0] = tr_data[0].str.replace("A","0")
-    tr_data[0] = tr_data[0].str.replace("C","1")
-    tr_data[0] = tr_data[0].str.replace("N","2")
-    tr_data[0] = tr_data[0].str.replace("G","3")
-    tr_data[0] = tr_data[0].str.replace("T","4")
-    tr_data.rename(columns = {0:'sequence', 1:'expression'}, inplace = True)
-    print(tr_data)
+    # below is the code to pad but it is giving an error
+    # tr_data[0] = fixed_len(tr_data[0], 112, "start", "X")
+    tr_data[0] = tr_data[0].str.replace("A", "0")
+    tr_data[0] = tr_data[0].str.replace("C", "1")
+    tr_data[0] = tr_data[0].str.replace("N", "2")
+    tr_data[0] = tr_data[0].str.replace("G", "3")
+    tr_data[0] = tr_data[0].str.replace("T", "4")
+    # pad the data
+    # print(tr_data[0])
+    tr_data.rename(columns={0: 'sequence', 1: 'expression'}, inplace=True)
     tr_data = tr_data.to_dict('records')
-    tr_dataset = SmartCacheDataset(tr_data, transform=OneHotEncoding(keys="sequence"), replace_rate=replace_rate, cache_rate=cache_rate)
+    tr_dataset = SmartCacheDataset(tr_data, transform=OneHotEncoding(keys="sequence"), replace_rate=replace_rate,
+                                   cache_rate=cache_rate)
     return tr_dataset
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     tr_dataset = get_dataset(max_sample_bytes=30000)
     # Next steps: implement batch > 1 (with padding) and flipping as augmentation
     for item in torch.utils.data.DataLoader(tr_dataset, batch_size=1):
